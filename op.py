87c8ee880510de1876e7a8885fb28877a7a41cc4
1C0D <63018905+1C0D@users.noreply.github.com>
2021-07-19 16:34:34 +0200

    Update op.py

--- op.py
+++ op.py
@@ -1,17 +1,29 @@
 import bpy
+from .utils import ChangeName
+
+debug = 1 ## 
 
 class SimpleOperator(bpy.types.Operator):
     """Tooltip"""
     bl_idname = "object.simple_operator"
     bl_label = "Simple Object Operator"
+    bl_options = {'UNDO'}    
 
     def execute(self, context):
 
-        bpy.ops.transform.resize(value=(2, 2, 1))
+        print('aaaa')
+        ChangeName.print_name() # try a change in utils
 
         return {'FINISHED'}
 
+
 def register():
+    
+    if debug:
+        import importlib
+        from . import utils # doing from .utils import ChangeName, the module is not imported, so we couldn't reload it
+        importlib.reload(utils) ## 
+    
     bpy.utils.register_class(SimpleOperator)
 
 def unregister():
